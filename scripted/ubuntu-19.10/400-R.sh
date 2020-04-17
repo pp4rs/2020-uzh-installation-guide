56f5a2feec1eb572be53db31bf47e149bbb18c48
Ulrich Bergmann <ulrich.bergmann@gmail.com>
2020-04-17 13:22:56 +0200

    Added line to get missing public sign key for R repo

--- scripted/ubuntu-19.10/400-R.sh
+++ scripted/ubuntu-19.10/400-R.sh
@@ -4,6 +4,10 @@
 echo "--- PP4RS 2020 R Installation Script Start"
 
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+
+# in case sudo apt update throws unsigned error for R repo, execute the following. Key might differ
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9
+
 sudo apt update
 sudo apt install r-base  r-base-dev
 
--- scripted/ubuntu-19.10/ALL.sh
+++ scripted/ubuntu-19.10/ALL.sh
@@ -8,6 +8,10 @@ echo "--- PP4RS 2020 CLI Installation Script Start"
 echo "--- 1) Installing from apt (please confirm every prompt)"
 
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+
+# in case sudo apt update throws unsigned error for R repo, execute the following. Key might differ
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9
+
 sudo apt update 
 sudo apt upgrade
 sudo apt install libcurl4-gnutls-dev librtmp-dev texlive-latex-extra pandoc r-base  r-base-dev
